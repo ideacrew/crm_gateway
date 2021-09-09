86ae3317a32fd41bced0a740173f7792e7cb55bd
Zane Sandin <zanesandin@gmail.com>
2021-09-09 16:35:49 -0400

    REFS maintenance - Saves progress for family update.

--- app/operations/families/handle_family_update.rb
+++ app/operations/families/handle_family_update.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
-require File.join(Rails.root, "app", "operations", "transformers", "family_to", "family_to_account_and_contacts")
-require 'aca_entities/liib/aca_entities/'
-
 module Families
   # Invoke a Family update service and update/create their corresponding contact records
   class HandleFamilyUpdate
     include Dry::Monads[:result, :do, :try]
     include EventSource::Command
+    require File.join(Rails.root, "app", "operations", "transformers", "family_to", "family_to_account_and_contacts")
+    require 'dry/monads'
+    require 'dry/monads/do'
+    require 'aca_entities/crms/accounts/account'
+    require 'aca_entities/crms/contracts/accounts/account_contract'
+    require 'aca_entities/crms/contracts/contacts/contact_contract'
+    require 'aca_entities/crms/contacts/contact'
+
 
     # @return [Dry::Monads::Result]
     def call(family_payload)
@@ -24,8 +29,15 @@ module Families
     end
 
     def validate_contacts_and_accounts(initialized_contacts_and_accounts)
-      binding.irb
-      result = Accounts::AccountContract.new.call(value)
+      # result = AcaEntities::Crms::Accounts::AccountContract.new.call(value)
+      account_params = initialized_contacts_and_accounts.except(:contacts)
+      account_validation = Crms::Accounts::AccountContract.new.call(account_params)
+      raise("Invalid account") if account_validation.failure?
+      contact_params = initialized_contacts_and_accounts[:contacts]
+      contact_params.each do |contact_hash|
+        contact_validation = Crms::Contacts::ContactContract.new.call(contact_hash)
+        raise("Invalid contact") if contact_validation.failure?
+      end
     end
   end
 end
