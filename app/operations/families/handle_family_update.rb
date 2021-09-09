70e503cee5243c78dcd1bdf868c518c06925b93c
Zane Sandin <zanesandin@gmail.com>
2021-09-09 12:25:24 -0400

    REFS maintenance - begins handle famly update spece.

--- app/operations/families/handle_family_update.rb
+++ app/operations/families/handle_family_update.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require File.join(Rails.root, "app", "operations", "transformers", "family_to", "family_to_account_and_contacts")
+require 'aca_entities/liib/aca_entities/'
+
 module Families
   # Invoke a Family update service and update/create their corresponding contact records
   class HandleFamilyUpdate
@@ -7,27 +10,22 @@ module Families
     include EventSource::Command
 
     # @return [Dry::Monads::Result]
-    def call(payload)
-      validated_family = yield validate_family(payload)
-      initialized_contacts_and_accounts = build_accounts_and_contacts(validated_family)
+    def call(family_payload)
+      initialized_contacts_and_accounts = build_accounts_and_contacts(family_payload)
       result = validate_contacts_and_accounts(initialized_contacts_and_accounts)
       Success(result)
     end
 
     protected
 
-    def validate_family(payload)
-      result = AcaEntities::Families::Family.new.call(payload.value!)
-      result.success? ? result : Failure(result.failure.errors.to_h)
-    end
-
-    # This is from sugar CRM stuff
-    def build_accounts_and_contacts(validated_family)
-      initialized_contacts_and_accounts = Operations::Transformers::FamilyTo::AccountAndContacts.new.call(validated_family.value!)
+    # Famlily Payload is a Hash
+    def build_accounts_and_contacts(family_payload)
+      initialized_contacts_and_accounts = Operations::Transformers::FamilyTo::AccountAndContacts.new.call(family_payload.to_h)
     end
 
     def validate_contacts_and_accounts(initialized_contacts_and_accounts)
-
+      binding.irb
+      result = Accounts::AccountContract.new.call(value)
     end
   end
 end
