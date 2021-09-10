3908a05195f7e9a98423d1b864a6dfc45ff17b75
Zane Sandin <zanesandin@gmail.com>
2021-09-10 15:45:18 -0400

    REFS maintenance - filters out username.

--- spec/support/config/vcr_setup.rb
+++ spec/support/config/vcr_setup.rb
@@ -4,7 +4,11 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/support/vcr_cassettes'
   c.hook_into :faraday
   c.default_cassette_options = { :record => :once }
-  
+
+  c.filter_sensitive_data('<SUGAR_CRM_USERNAME>') do
+    Rails.application.config.sugar_crm['username']
+  end
+
   c.filter_sensitive_data('<SUGAR_CRM_PASSWORD>') do
     Rails.application.config.sugar_crm['password']
   end
