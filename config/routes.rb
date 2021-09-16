f2442fb43a194e3e609d9e6d5e98a0244daee54c
Bill Transue <transue@gmail.com>
2021-09-16 09:37:13 -0400

    Update routes for new Event view and action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'events#index'
+
+  resources :events, only: [ :show ] do
+    get :retry, on: :member
+  end
 end
