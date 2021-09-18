9f7d86d70ede515cdaaad8cd425bcd138d6b62a6
Bill Transue <transue@gmail.com>
2021-09-18 19:13:14 -0400

    Fix typo in views

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,6 +3,6 @@
 # Namespace container Events view helpers
 module EventsHelper
   def event_row(event)
-    safe_join(event.data[:family_members].map { |p| p[:person][:person_name][:full_name] }, tar('br'))
+    safe_join(event.data[:family_members].map { |p| p[:person][:person_name][:full_name] }, tag('br'))
   end
 end
