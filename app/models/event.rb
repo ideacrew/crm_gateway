eb25186c52b2fcceba42bde57893a2974cd23c6f
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2023-09-26 15:09:59 -0400

    comment all the code related to morphs

--- app/models/event.rb
+++ app/models/event.rb
@@ -33,10 +33,6 @@ class Event
       transitions from: :received, to: :processing
     end
 
-    event :fail do
-      transitions from: :processing, to: :failure
-    end
-
     event :complete do
       transitions from: :processing, to: :successful
     end
@@ -84,60 +80,60 @@ class Event
   #   show_morph
   # end
 
-  def archive_morph
-    cable_ready['events'].remove(
-      selector: "#event-row-#{id}"
-    )
-    cable_ready.broadcast
-  end
+  # def archive_morph
+  #   cable_ready['events'].remove(
+  #     selector: "#event-row-#{id}"
+  #   )
+  #   cable_ready.broadcast
+  # end
 
-  def update_morph
-    show_morph
-  end
+  # def update_morph
+  #   show_morph
+  # end
 
-  def row_morph
-    row_html = ApplicationController.render(
-      partial: "events/row",
-      locals: { event: self }
-    )
+  # def row_morph
+  #   row_html = ApplicationController.render(
+  #     partial: "events/row",
+  #     locals: { event: self }
+  #   )
 
-    cable_ready["events"].morph(
-      selector: "#event-row-#{id}",
-      html: row_html
-    )
+  #   cable_ready["events"].morph(
+  #     selector: "#event-row-#{id}",
+  #     html: row_html
+  #   )
 
-    cable_ready.broadcast
-  end
+  #   cable_ready.broadcast
+  # end
 
-  def show_morph
-    show_html = ApplicationController.render(
-      partial: "events/event",
-      locals: { event: self }
-    )
+  # def show_morph
+  #   show_html = ApplicationController.render(
+  #     partial: "events/event",
+  #     locals: { event: self }
+  #   )
 
-    cable_ready["events"].morph(
-      selector: "#event-#{id}",
-      html: show_html
-    )
+  #   cable_ready["events"].morph(
+  #     selector: "#event-#{id}",
+  #     html: show_html
+  #   )
 
-    cable_ready.broadcast
-  end
+  #   cable_ready.broadcast
+  # end
 
   # after_create do
   #   create_morph
   # end
 
-  def create_morph
-    row_html = ApplicationController.render(
-      partial: "events/row",
-      locals: { event: self }
-    )
+  # def create_morph
+  #   row_html = ApplicationController.render(
+  #     partial: "events/row",
+  #     locals: { event: self }
+  #   )
 
-    cable_ready['events'].prepend(
-      selector: 'table tbody',
-      html: row_html
-    )
+  #   cable_ready['events'].prepend(
+  #     selector: 'table tbody',
+  #     html: row_html
+  #   )
 
-    cable_ready.broadcast
-  end
+  #   cable_ready.broadcast
+  # end
 end
